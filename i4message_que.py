8c2d31c0f5d9a4512e37588f5cf2cb2c670b0687
greatabel <myreceiver2for2github@gmail.com>
2020-02-26 17:24:40 +0800

    i5simulate_rstp/

--- i4message_que.py
+++ i4message_que.py
@@ -11,11 +11,11 @@ class producer(multiprocessing.Process):
 
     def run(self):
         global my_queue_len
-        for i in range(5):
+        for i in range(15):
 
             item = random.randint(0, 256)
             self.queue.put(item) 
-            print ("Process Producer : 消息 %d 添加到 queue %s"\
+            print ("模拟摄像头检测 Process Producer : 消息 %d 添加到 queue %s"\
                    % (item,self.name))
             time.sleep(1)
 
@@ -30,13 +30,13 @@ class consumer(multiprocessing.Process):
 
         while True:
             if (self.queue.empty()):
-                print("the queue is empty")
+                print("队列已空")
                 break
             else :
                 time.sleep(0.5)
                 item = self.queue.get()
 
-                print ('Process Consumer : 消息 %d 通知到相应人员 from by %s \n'\
+                print ('Process调用程序 : 消息 %d 通知到相应人员 from by %s \n'\
                        % (item, self.name))
                 time.sleep(1)
 
