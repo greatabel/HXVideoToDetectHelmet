bf487ce41604d15ca53522c9438a406754fef3e5
greatabel <myreceiver2for2github@gmail.com>
2020-03-03 20:44:53 +0800

    i6combine_multiple_camera.py

--- i6combine_multiple_camera.py
+++ i6combine_multiple_camera.py
@@ -1,14 +1,17 @@
 import multiprocessing
+
 import random
 import time
-import cv2
-import numpy as np
-import urllib.request
+
 
 
 pre_video_url = 'http://0.0.0.0:5000/video_feed'
 
-def show_video(video_url):
+def show_video(video_url, queue):
+    import cv2
+    import numpy as np
+    import urllib.request
+    print('show_video')
     stream = urllib.request.urlopen(video_url)
     total_bytes = b''
     while True:
@@ -20,38 +23,26 @@ def show_video(video_url):
             total_bytes= total_bytes[b+2:] # other informations
             
             # decode to colored image ( another option is cv2.IMREAD_GRAYSCALE )
-            img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR) 
+            img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+
             # print(type(img))
             # cv2.imshow('Window name',cv2.flip(img, -1)) # display image while receiving data
+
             cv2.imshow('frame',img)
+            print('simulate detect', video_url)
+
             if cv2.waitKey(1) ==27: # if user hit esc            
                 break
     cv2.destroyWindow()
 
-class producer(multiprocessing.Process):
-    def __init__(self, queue, number):
-        multiprocessing.Process.__init__(self)
-        self.queue = queue
-        self.number = number
 
-    def run(self):
-        global my_queue_len
-        print(self.number, ' run')
-        # for i in range(3):
-        #     url = pre_video_url + str(i)
-
-
-        #     item = random.randint(0, 256)
-        #     self.queue.put(item) 
-        #     print ("模拟摄像头检测 Process Producer : 消息 %d 添加到 queue %s"\
-        #            % (item,self.name))
-        #     time.sleep(1)
 
 
 
 class consumer(multiprocessing.Process):
     def __init__(self, queue):
         multiprocessing.Process.__init__(self)
+
         self.queue = queue
 
     def run(self):
@@ -71,14 +62,20 @@ class consumer(multiprocessing.Process):
 if __name__ == '__main__':
         queue = multiprocessing.Queue()
 
-        producers = []
-        for i in range(3):
-            print('#',i)
-            producers.append(producer(queue,i))
+        cam_process0 = multiprocessing.Process(target=show_video,args=(pre_video_url+str(0),queue))
+        cam_process0.start()
+
+        cam_process1 = multiprocessing.Process(target=show_video,args=(pre_video_url+str(1),queue))
+        cam_process1.start()
+
+        producers = [cam_process0, cam_process1]
+        # for i in range(3):
+        #     print('#',i)
+        #     producers.append(producer(queue,i))
         # process_producer = producer(queue, 0)
         process_consumer = consumer(queue)
-        for p in producers:
-            p.start()
+        # for p in producers:
+        #     p.start()
         # process_producer.start()
         process_consumer.start()
         for p in producers:
