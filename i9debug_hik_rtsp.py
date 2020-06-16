3461dd56ef38a4a3604a2e4851ac53a347fb68ad
greatabel <myreceiver2for2github@gmail.com>
2020-06-16 15:27:37 +0800

    add another hub

--- i9debug_hik_rtsp.py
+++ i9debug_hik_rtsp.py
@@ -2,7 +2,10 @@ import cv2
 # 海康
 # 12345
 # yxgl$666
+# yxgl!123456
 # yxgl123456
+
+
 # input_str= 'rtsp://admin:yxgl$666@192.168.200.182:554'
 # input_str= 'rtsp://admin:yxgl$666@192.168.200.183:554'
 # input_str= 'rtsp://admin:yxgl$666@192.168.200.184:554'
@@ -39,7 +42,109 @@ import cv2
 # input_str= 'rtsp://admin:yxgl$666@192.168.200.199:554'
 # input_str= 'rtsp://admin:yxgl$666@192.168.200.204:554'
 
-input_str= 'rtsp://admin:yxgl$666@192.168.200.205:554'
+
+# input_str= 'rtsp://admin:12345@192.168.200.51:554'
+# input_str= 'rtsp://admin:12345@192.168.200.52:554'
+# input_str= 'rtsp://admin:12345@192.168.200.53:554'
+# input_str= 'rtsp://admin:12345@192.168.200.54:554'
+# input_str= 'rtsp://admin:12345@192.168.200.55:554'
+# input_str= 'rtsp://admin:12345@192.168.200.56:554'
+
+# wait
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.57:554'
+
+# input_str= 'rtsp://admin:12345@192.168.200.58:554'
+# input_str= 'rtsp://admin:12345@192.168.200.59:554'
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.60:554'
+# input_str= 'rtsp://admin:12345@192.168.200.61:554'
+
+# not online:
+# input_str= 'rtsp://admin:12345@192.168.200.62:554'
+
+
+# input_str= 'rtsp://admin:12345@192.168.200.63:554'
+# input_str= 'rtsp://admin:12345@192.168.200.64:554'
+# 大厅：
+# input_str= 'rtsp://admin:12345@192.168.200.65:554'
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.66:554'
+# input_str= 'rtsp://admin:12345@192.168.200.67:554'
+
+# not online:
+# input_str= 'rtsp://admin:12345@192.168.200.68:554'
+
+# input_str= 'rtsp://admin:12345@192.168.200.70:554'
+# input_str= 'rtsp://admin:12345@192.168.200.71:554'
+# 传送带
+# input_str= 'rtsp://admin:12345@192.168.200.72:554'
+# 监控 煤区
+# input_str= 'rtsp://admin:12345@192.168.200.73:554'
+# input_str= 'rtsp://admin:12345@192.168.200.74:554'
+# input_str= 'rtsp://admin:12345@192.168.200.75:554'
+# 山脚下
+# input_str= 'rtsp://admin:12345@192.168.200.76:554'
+# 水处理
+# input_str= 'rtsp://admin:12345@192.168.200.77:554'
+# input_str= 'rtsp://admin:12345@192.168.200.78:554'
+# input_str= 'rtsp://admin:12345@192.168.200.79:554'
+# input_str= 'rtsp://admin:12345@192.168.200.80:554'
+# 楼梯
+# input_str= 'rtsp://admin:12345@192.168.200.81:554'
+# input_str= 'rtsp://admin:12345@192.168.200.82:554'
+# input_str= 'rtsp://admin:12345@192.168.200.83:554'
+
+# not online
+# input_str= 'rtsp://admin:12345@192.168.200.84:554'
+# 监控室
+# input_str= 'rtsp://admin:12345@192.168.200.85:554'
+# input_str= 'rtsp://admin:yxgl123456@192.168.200.86:554'
+# 楼道
+# input_str= 'rtsp://admin:12345@192.168.200.87:554'
+# 机器旁边
+# input_str= 'rtsp://admin:12345@192.168.200.88:554'
+
+#  not online
+# input_str= 'rtsp://admin:12345@192.168.200.89:554'
+
+# input_str= 'rtsp://admin:12345@192.168.200.90:554'
+# 沙漏旁边
+# input_str= 'rtsp://admin:12345@192.168.200.91:554'
+# input_str= 'rtsp://admin:12345@192.168.200.92:554'
+# 某办公室
+# input_str= 'rtsp://admin:12345@192.168.200.93:554'
+
+#not online:
+# input_str= 'rtsp://admin:12345@192.168.200.94:554'
+# 泥浆
+# input_str= 'rtsp://admin:12345@192.168.200.95:554'
+# input_str= 'rtsp://admin:12345@192.168.200.96:554'
+# 水洗机器旁
+# input_str= 'rtsp://admin:12345@192.168.200.97:554'
+# input_str= 'rtsp://admin:12345@192.168.200.98:554'
+
+# wait
+# input_str= 'rtsp://admin:yxgl!123456@192.168.200.99:554'
+# 地磅
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.101:554'
+# input_str= 'rtsp://admin:12345@192.168.200.102:554'
+
+# not line
+# input_str= 'rtsp://admin:12345@192.168.200.103:554'
+
+# input_str= 'rtsp://admin:12345@192.168.200.105:554'
+# 江边
+# input_str= 'rtsp://admin:12345@192.168.200.106:554'
+# 进厂
+# input_str= 'rtsp://admin:12345@192.168.200.107:554'
+# input_str= 'rtsp://admin:12345@192.168.200.109:554'
+
+# 旋转的水上
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.110:554'
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.111:554'
+# 电机室
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.112:554'
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.113:554'
+# input_str= 'rtsp://admin:yxgl$666@192.168.200.114:554'
+input_str= 'rtsp://admin:yxgl$666@192.168.200.114:554'
 
 
 #rtsp://admin:yxgl!123456@10.249.181.9:554/Streaming/Channels/101
