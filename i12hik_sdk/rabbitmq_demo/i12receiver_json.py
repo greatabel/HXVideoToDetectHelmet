a1b887c419a63b54880c3fb8a0296ac7da27f40d
greatabel <myreceiver2for2github@gmail.com>
2020-09-21 11:40:05 +0800

    decode base64

--- i12hik_sdk/rabbitmq_demo/i12receiver_json.py
+++ i12hik_sdk/rabbitmq_demo/i12receiver_json.py
@@ -1,5 +1,6 @@
 import pika
 import simplejson as json
+import base64
 #https://stackoverflow.com/questions/50404273/python-tutorial-code-from-rabbitmq-failing-to-run
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -7,9 +8,14 @@ channel = connection.channel()
 
 
 channel.queue_declare(queue='hello')
-
+filename = 'received_test.jpg'
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % json.loads(body))
+    msg = json.loads(body)
+    print(" [x] Received %r" % msg)
+    imgdata = base64.b64decode(msg['img'])
+    print(type(imgdata), '#'*10)
+    # with open(filename, 'wb') as f:
+    #     f.write(imgdata)
 
 channel.basic_consume(on_message_callback=callback,
                       queue='hello',
