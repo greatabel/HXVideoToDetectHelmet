ffb49f358c7ff8d2ae670282b0b3730499c6fa19
greatabel <myreceiver2for2github@gmail.com>
2020-09-29 10:14:30 +0800

    remote rabbitmq

---
+++ i13hik_sdk_mq/i13sender_remote.py
@@ -0,0 +1,21 @@
+import pika
+
+
+host = '10.248.68.227'
+credentials = pika.PlainCredentials('test', 'test')
+parameters = pika.ConnectionParameters(host,
+                                   5672,
+                                   '/',
+                                   credentials)
+
+connection = pika.BlockingConnection(parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='hello0')
+
+channel.basic_publish(exchange='',
+                  routing_key='hello0',
+                  body='Hello W0rld from macbook pro!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
