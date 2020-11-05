3f7d19883daed0d49f45d39e172c10027eaa25c9
greatabel <myreceiver2for2github@gmail.com>
2020-11-05 19:28:21 +0800

    change in ai-server-1

--- lifejacket/scenes/config_file.py
+++ lifejacket/scenes/config_file.py
@@ -12,7 +12,8 @@ config = {
     #####Queues#####
     'mq_username': 'test',
     'mq_pswd': 'test',  
-    'mq_server_host': '127.0.0.1',
+   # 'mq_server_host': '127.0.0.1',
+    'mq_server_host': '10.248.68.249',
     'mq_server_port': '5672',
     'frame_q_in': 'LifeJacket',
     'frame_q_out':'hx_jacket_out',
--- safetybelt/scenes/config_file.py
+++ safetybelt/scenes/config_file.py
@@ -11,7 +11,8 @@ config = {
     #####Queues#####
     'mq_username': 'test',
     'mq_pswd': 'test',  
-    'mq_server_host': '127.0.0.1',
+    # 'mq_server_host': '127.0.0.1',
+    'mq_server_host': '10.248.68.249',
     'mq_server_port': '5672',
     'frame_q_in': 'SafetyBelt',
     'frame_q_out':'hx_safetybelet_out',
