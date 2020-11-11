2ffe05dfed0fe7d55656c1fd80e7f987b899c3f3
greatabel <myreceiver2for2github@gmail.com>
2020-11-11 13:19:53 +0800

    jacket

--- i15jacket_and_belt/scenes/config_file.py
+++ i15jacket_and_belt/scenes/config_file.py
@@ -18,8 +18,8 @@ config = {
     #####Queues#####
     'mq_username': 'test',
     'mq_pswd': 'test',  
-    # 'mq_server_host': '10.248.68.249',
-    'mq_server_host': '127.0.0.1',
+    'mq_server_host': '10.248.68.249',
+    # 'mq_server_host': '127.0.0.1',
     'mq_server_port': '5672',
     'lifeJacket_q_in': 'LifeJacket',
     # 'lifeJacket_q_out':'lifeJacket_out',
--- servers.md
+++ servers.md
@@ -3,6 +3,6 @@
 528688914    阳新水泥 虚拟机, system_ps:temp!2020 
 306520036    阳新水泥 ai_server0
 594321116    阳新水泥 ai_server1
-594534185    阳新水泥 ai_server2
+594534185    阳新水泥 ai_server2 jacket/belt
 
 202417081    myself ubunutu notebook
