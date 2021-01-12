50126562929e442eb7f265920b4bccc551577c02
greatabel <myreceiver2for2github@gmail.com>
2021-01-12 15:00:58 +0800

    change to test common url

--- i0camera_test.py
+++ i0camera_test.py
@@ -9,8 +9,10 @@
 import cv2
 # cap = cv2.VideoCapture("rtsp://admin:test1024@10.248.10.111:554/h264/ch33/main/av_stream")
 # cap = cv2.VideoCapture("rtsp://admin:huaxin12345@10.248.10.43:554/Streaming/Channels/1")
+url = 'rtsp://admin:abcd1234@10.250.4.149:554/Streaming/Channels/101'
+# cap = cv2.VideoCapture("rtsp://admin:huaxin12345@10.248.10.111:554/h264/ch33/main/av_stream")
+cap = cv2.VideoCapture(url)
 
-cap = cv2.VideoCapture("rtsp://admin:huaxin12345@10.248.10.111:554/h264/ch33/main/av_stream")
 # cap = cv2.VideoCapture("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov")
 
 # cap = cv2.VideoCapture("rtsp://admin:yxgl$666@192.168.200.215:554/Streaming/Channels/1")
